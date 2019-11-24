5345ee8189e8d8ee087c1a8904fe3beaccf9e3d8
te00141 <te00141@surrey.ac.uk>
2019-11-24 15:01:47 +0000

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#home'
   post 'submitted_contact', to:'home#submitted'
   get 'contact',  to: 'home#contact'
   resources :locations
 
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
